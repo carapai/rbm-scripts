6ba21d6d81a437eb2b234e05784ce4e7108a3709
carapai <olupotcharles@gmail.com>
2018-12-06 17:37:15 +0300

    Test with dev instance

--- src/lands-quarterly.js
+++ src/lands-quarterly.js
@@ -1,6 +1,8 @@
 const soap = require('./soap');
 const utils = require('./utils');
 
+const _ = require('lodash');
+
 
 const dataSet = require('./lands-quarterly-mapping.json');
 
@@ -10,15 +12,17 @@ const processQuarterly = async () => {
     const data = await soap.getLAM08('2017Q1');
     const dataValues = utils.processData(dataSet, data['return']);
 
-    try {
+    return _.uniqWith(dataValues, _.isEqual);
+
+   /* try {
         const response = await utils.insertData({dataValues});
         return response;
     } catch (e) {
        return e
-    }
+    }*/
 
 };
 
 processQuarterly().then(response => {
-    console.log(JSON.stringify(response, null, 2));
+    console.log(JSON.stringify(response));
 });
