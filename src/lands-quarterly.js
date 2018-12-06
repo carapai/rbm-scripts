a477b0a23cebc45ae6761e8873d3d2967e17a9f8
carapai <olupotcharles@gmail.com>
2018-12-06 13:30:40 +0300

    Debuging

--- src/lands-quarterly.js
+++ src/lands-quarterly.js
@@ -1,7 +1,5 @@
-const rq = require('request-promise');
 const soap = require('./soap');
 const utils = require('./utils');
-const winston = require('./winston');
 
 
 const dataSet = require('./lands-quarterly-mapping.json');
@@ -10,7 +8,7 @@ const dataSet = require('./lands-quarterly-mapping.json');
 const processQuarterly = async () => {
 
     const data = await soap.getLAM08('2017Q1');
-
+    console.log(data);
     const dataValues = utils.processData(dataSet, data);
 
     return dataValues;
