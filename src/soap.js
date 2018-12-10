a02477313dc35ff6c3f072303940d964d2368ec7
carapai <olupotcharles@gmail.com>
2018-12-10 20:04:07 +0300

    updated url

--- src/soap.js
+++ src/soap.js
@@ -5,8 +5,10 @@ module.exports.getLAM08 = period => {
     const args = {period: period};
     return new Promise((resolve, reject) => {
         soap.createClient(url, (err, client) => {
+            console.log(err)
             if (err) reject(err);
             client.getlistOfLAM08(args, (err, result) => {
+                console.log(err);
                 if (err) reject(err);
                 else resolve(result);
             });
