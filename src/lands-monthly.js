6066e731e2369f73b996aada1c98bab1f874a728
carapai <olupotcharles@gmail.com>
2018-12-11 12:16:01 +0300

    Monthly data

--- src/lands-monthly.js
+++ src/lands-monthly.js
@@ -7,8 +7,8 @@ const dataSet = require('./lands-monthly-mapping.json');
 
 const fmt = 'YYYYMM';
 const processMonthly = async () => {
-    const period = moment().subtract(1, 'M').format(fmt);
-    // const period = '201803';
+    // const period = moment().subtract(1, 'M').format(fmt);
+    const period = '201803';
     try {
         const allData = await soap.getLAM12(period);
         if (allData.code) {
@@ -17,8 +17,9 @@ const processMonthly = async () => {
             const dataValues = utils.processData(dataSet, [allData]);
             const processedData = _.uniqWith(dataValues, _.isEqual);
             return processedData;
+            // return await utils.insertData({dataValues: processedData});
+
         }
-        // return await utils.insertData({dataValues: processedData});
     } catch (e) {
         // console.log('Error');
         return e
